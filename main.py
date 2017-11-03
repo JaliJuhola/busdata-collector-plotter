d18b21d0196c5b2780a6772c3742c4da9e60cd20
JaliJuhola <jalijuhola@gmail.com>
2017-11-03 15:15:31 -0700

    main and busdata updated

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from busdata import collectData
+from routespeeds import writeSpeeds
 
 import sys
 
-
+# Collecting data about bus number @argv[3] for @argv[1] seconds with @argv[2] intervall 
 if len(sys.argv) == 4 and sys.argv[1].isdigit() and sys.argv[2].isdigit():
-   collectData((int)(sys.argv[1]),(int)(sys.argv[1]), str(sys.argv[3]))
+   collectData((int)(sys.argv[1]),(int)(sys.argv[2]), str(sys.argv[3]))
 else:
    #using default values
-   collectData(40, 5, '6')
+   collectData(100, 5, '6')
+
+writeSpeeds('tampere.png', 'tampere1.png')
